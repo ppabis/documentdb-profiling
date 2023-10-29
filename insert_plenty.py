a3365853c9e157558c54bba30b1a0070dbd91001
ppabis <piotr@pabis.eu>
2023-10-29 15:27:45 +0100

    add script processor to python scripts

--- insert_plenty.py
+++ insert_plenty.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import pymongo, requests, uuid, random, os
 
 """
--- query_much.py
+++ query_much.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import pymongo, requests, uuid, random, os, time
 
 MONGO_URL = os.environ.get('MONGO_URL')
