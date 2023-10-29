0ed4a0f3eab91e69390c0fe06655e154b13fad82
ppabis <piotr@pabis.eu>
2023-10-29 10:56:30 +0100

    install requests

--- user_data.sh
+++ user_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo yum install -y python3-pip
-sudo pip3 install pymongo
+sudo pip3 install pymongo requests
 sudo mkdir -p /usr/local/share/ca-certificates/
 sudo wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem -O /usr/local/share/ca-certificates/rds.pem
 sudo chmod 0644 /usr/local/share/ca-certificates/rds.pem
